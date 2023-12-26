b4f184d97c2d06e8fb592e238a23332c8184ce7a
Jonathan Washburn <jrwashburn@Jonathans-MacBook-Pro.local>
2023-12-26 09:12:22 -0600

    support systemd

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 CURL=/usr/bin/curl
 SCRIPT=discord_bot.sh
-SCRIPT_URL=https://raw.githubusercontent.com/t4top/mina-node-discord-bot/main/$SCRIPT
+SCRIPT_URL=https://raw.githubusercontent.com/jrwashburn/mina-node-discord-bot/main/$SCRIPT
 
 # prompt for webhook url
 while true; do
@@ -26,8 +26,43 @@ sed -i "s@ENTER_DISCORD_WEBHOOK_URL_HERE@$WEBHOOK_URL@g" $SCRIPT
 # make the script executable
 chmod +x $SCRIPT
 
-# add the script to cronjob
-(crontab -l | grep $PWD/$SCRIPT) || (crontab -l 2>/dev/null; echo "*/15 * * * * $PWD/$SCRIPT >/dev/null 2>&1") | crontab -
+sudo mv $SCRIPT /usr/local/bin/$SCRIPT
+
+read -n1 -r -p "Would you like to run under systemd instead of cron? (y/n) " YESNO
+echo
+if [ $YESNO = "y" ] || [ $YESNO = "Y" ] ; then
+  sudo tee /etc/systemd/user/mina-discord-bot.service &>/dev/null << E-O-F
+[Unit]
+Description=Mina Discord Bot Service
+After=network-online.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=15
+ExecStart=/usr/local/bin/$SCRIPT 
+
+[Install]
+WantedBy=default.target
+E-O-F
+
+  sudo tee /etc/systemd/user/mina-discord-bot.timer &>/dev/null << E-O-F
+  [Unit]
+Description=Mina Discord Bot Timer
+
+[Timer]
+OnCalendar=00/0:15
+Unit=mina-discord-bot.service
+Persistent=true
+
+[Install]
+WantedBy=default.target
+E-O-F
+
+else
+  # add the script to cronjob
+  (crontab -l | grep $PWD/$SCRIPT) || (crontab -l 2>/dev/null; echo "*/15 * * * * /usr/local/bin/$SCRIPT >/dev/null 2>&1") | crontab -
+fi
 
 # execute the script
-./$SCRIPT
+/usr/local/bin/$SCRIPT
